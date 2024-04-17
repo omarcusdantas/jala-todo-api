8c6c954cf2205a818cc5debcf422df66a496aaa0
MarcusDantas <marvinsd@gmail.com>
2024-04-17 14:44:22 -0300

    chore: add project basic structure

---
+++ Contracts/ITodoService.cs
@@ -0,0 +1,16 @@
+﻿using JalaTodoApi.Models;
+
+namespace JalaTodoApi.Contracts;
+
+public interface ITodoService
+{
+    Task<IEnumerable<Todo>> GetAll();
+
+    Task<Todo> Get(Guid id);
+
+    Task<Todo> Create(Todo todo);
+
+    Task<Todo> Update(Guid id, Todo todo);
+
+    Task<bool> Delete(Guid id);
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,19 @@
+﻿namespace JalaTodoApi.Models;
+
+public class Todo
+{
+    public Guid Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime DueDate { get; set; }
+
+    public bool OverDue { get; set; } = false;
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+}
+
