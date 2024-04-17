1e8df54af3274ea0ffa6c21dc868cb4f3fd8d87f
MarcusDantas <marvinsd@gmail.com>
2024-04-17 15:08:08 -0300

    feat: add dbcontext and in memory database with entity framework

---
+++ Database/TodoDBContext.cs
@@ -0,0 +1,9 @@
+﻿using JalaTodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JalaTodoApi.Database;
+
+public class TodoDBContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Todo> Todos { get; set; }
+}
---
+++ Repositories/TodoRepository.cs
@@ -0,0 +1,38 @@
+﻿using JalaTodoApi.Contracts;
+using JalaTodoApi.Database;
+using JalaTodoApi.Models;
+
+namespace JalaTodoApi.Repositories;
+
+public class TodoRepository(TodoDBContext dBContext) : ITodoRepository
+{
+    private readonly TodoDBContext _dbContext = dBContext;
+
+    public async Task<Todo> Create(Todo todo)
+    {
+        await _dbContext.Todos.AddAsync(todo);
+        await _dbContext.SaveChangesAsync();
+
+        return todo;
+    }
+
+    public Task<bool> Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Todo> Get(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Todo>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Todo> Update(Guid id, Todo todo)
+    {
+        throw new NotImplementedException();
+    }
+}
